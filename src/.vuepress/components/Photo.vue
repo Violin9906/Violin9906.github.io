53d9b5126558fee583fafbce8cf411acab325443
Ruolin Wang <30993803+Violin9906@users.noreply.github.com>
2025-09-28 22:34:17 +0800

    Initial commit

---
+++ src/.vuepress/components/Photo.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { usePageData, usePageFrontmatter } from "vuepress/client";
+
+const frontmatter = usePageFrontmatter();
+
+const photo = (() => {
+    return frontmatter.value.photo
+})()
+
+const alt = (() => {
+    return "photo"
+})()
+</script>
+
+<template>
+    <img v-if="photo" class="photo" :src="photo" :alt="alt">
+</template>
+
+<style>
+.photo {
+    margin-left: auto;
+    border-radius: 50%;
+    height: 8rem;
+    width: 8rem;
+    object-fit: cover;
+}
+</style>
